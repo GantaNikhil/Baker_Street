9d1def7118de3585600f840483a485d5867c465e
campusnews147 <campusnews147@gmail.com>
2023-04-08 01:20:05 +0530

    entrace pages layout

---
+++ Baker_Street/app/src/main/java/com/example/baker_street/activity_signin.kt
@@ -0,0 +1,11 @@
+package com.example.baker_street
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_signin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
