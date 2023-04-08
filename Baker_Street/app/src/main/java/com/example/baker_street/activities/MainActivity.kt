caf9dee8e33021af411e423bfe25e86e43bd81b0
GantaNikhil <gantanikhil003@gmail.com>
2023-04-08 05:30:05 +0530

    Empty Commit

--- Baker_Street/app/src/main/java/com/example/baker_street/activities/MainActivity.kt
+++ Baker_Street/app/src/main/java/com/example/baker_street/activities/MainActivity.kt
@@ -3,10 +3,25 @@ package com.example.baker_street.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.baker_street.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    //showFragment()
+                    true
+                }
+                R.id.navigation_profile -> {
+                    //showFragment()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
