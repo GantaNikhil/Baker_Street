28a39f4a86afb6e988f9174f5b37b13e8c7d732d
campusnews147 <campusnews147@gmail.com>
2023-04-08 05:59:08 +0530

    Minor Changes

--- Baker_Street/app/src/main/java/com/example/baker_street/activities/SignInActivity.kt
+++ Baker_Street/app/src/main/java/com/example/baker_street/activities/SignInActivity.kt
@@ -2,28 +2,50 @@ package com.example.baker_street.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.baker_street.R
+import com.example.baker_street.databinding.ActivitySigninBinding
+import com.example.baker_street.databinding.ActivitySignupBinding
 import com.example.baker_street.models.UserModel
 import com.example.baker_street.viewmodels.AuthViewModel
 
 class SignInActivity : AppCompatActivity() {
 
+
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var cnfpassword: EditText
+    private lateinit var binding:ActivitySigninBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
-
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val signinviewmodel = ViewModelProvider(this)[AuthViewModel::class.java]
 
         signinviewmodel.getMessageObserver()?.observe(this){
 
         }
 
+        binding.txtVForgotPassword.setOnClickListener{
+            val forgotPassword = Intent(this,ForgotPasswordActivity::class.java)
+            startActivity(forgotPassword)
+        }
+
+        binding.txtVSignUp.setOnClickListener{
+            val toSignUp = Intent(this,SignUpActivity::class.java)
+            startActivity(toSignUp)
+        }
 //        signup.setOnClickListener{
 //
 //        }
+    }
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAffinity()
     }
 }
--- Baker_Street/app/src/main/java/com/example/baker_street/activities/SignUpActivity.kt
+++ Baker_Street/app/src/main/java/com/example/baker_street/activities/SignUpActivity.kt
@@ -1,7 +1,9 @@
 package com.example.baker_street.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -11,19 +13,30 @@ import com.example.baker_street.models.UserModel
 import com.example.baker_street.viewmodels.AuthViewModel
 
 class SignUpActivity :AppCompatActivity(){
+
+    private lateinit var email:EditText
+    private lateinit var password:EditText
+    private lateinit var cnfpassword:EditText
+    private lateinit var signupviewmodel:AuthViewModel
     private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val email = binding.txtEEmail
-        val password = binding.txtEPassword
-        val cnfpassword = binding.txtEConfirmPassword
-        val signupviewmodel = ViewModelProvider(this)[AuthViewModel::class.java]
+         email = binding.txtEEmail
+         password = binding.txtEPassword
+         cnfpassword = binding.txtEConfirmPassword
+
+         signupviewmodel = ViewModelProvider(this)[AuthViewModel::class.java]
 
         binding.btnSignUp.setOnClickListener {
             signupviewmodel.signUpUser(UserModel(email.text.toString(), password.text.toString(),cnfpassword.text.toString()),"")
+        }
+
+        binding.goToLogin.setOnClickListener{
+            val toSignIn = Intent(this,SignInActivity::class.java)
+            startActivity(toSignIn)
         }
 
         signupviewmodel.getMessageObserver()?.observe(this){
