b3074daad8e5e5ab1889f518c0a5d630c484691a
campusnews147 <campusnews147@gmail.com>
2023-04-08 11:30:18 +0530

    "Splash done"

--- Baker_Street/app/src/main/java/com/example/baker_street/activities/SplashActivity.kt
+++ Baker_Street/app/src/main/java/com/example/baker_street/activities/SplashActivity.kt
@@ -2,19 +2,26 @@ package com.example.baker_street.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.baker_street.R
 import java.util.*
 
 
 class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT = 800
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        Timer().schedule(object : TimerTask() {
-            override fun run() {
-                startActivity(Intent(applicationContext, SignInActivity::class.java))
-            }
-        }, 2000)
+//        Timer().schedule(object : TimerTask() {
+//            override fun run() {
+//                startActivity(Intent(applicationContext, SignInActivity::class.java))
+//            }
+//        }, 2000)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT.toLong())
     }
 }
